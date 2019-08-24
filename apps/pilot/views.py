dcb0801c355919ac639f2f7898fa081e2a099659
Juan Martinez <juanmartinezv6@gmail.com>
2019-08-23 23:26:29 -0700

    styled the site using materialize, just got to a clickable prototype

--- apps/pilot/views.py
+++ apps/pilot/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def index(request):
+    return render(request, "pilot/index.html")
+
+def calculate(request):
+    p = request.POST
+
+    if request.method == 'POST':
+
+        wind = p['wind']
+        speed = p['speed']
+        depart = p['departing']
+        arrive = p['arriving']
+        
+        if speed == '':
+            speed = 140
+
+        print(wind)
+        print(speed)
+        print(depart)
+        print(arrive)
+
+        return redirect("/")
