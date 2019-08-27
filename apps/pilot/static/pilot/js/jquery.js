700a8c9537ddfb6545f3fc0368d30771385f2b18
Juan Martinez <juanmartinezv6@gmail.com>
2019-08-26 22:34:02 -0700

    added the google maps api

--- apps/pilot/static/pilot/js/jquery.js
+++ apps/pilot/static/pilot/js/jquery.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
 
     $("select").formSelect();
 
+    var map;
+    window.initMap = function() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 37.0902, lng: -95.7129},
+            zoom: 3.5,
+            disableDefaultUI: true
+        });
+    }
+    
 });
