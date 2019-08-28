675b3835f9f6aeb41cafc462d49ecbd3bc5f252c
Juan Martinez <juanmartinezv6@gmail.com>
2019-08-28 16:12:45 -0700

    added waypoints to the airports the user chooses

--- apps/pilot/static/pilot/js/jquery.js
+++ apps/pilot/static/pilot/js/jquery.js
@@ -1,14 +1,67 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+    console.log(depart, arrive);
 
     $("select").formSelect();
 
     var map;
-    window.initMap = function() {
+    var service;
+    var infowindow;
+
+    window.initMap = initMap;
+
+    function initMap() {
+        var california = new google.maps.LatLng(37.0902, -102.7129);
+
+        infowindow = new google.maps.InfoWindow();
+
         map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 37.0902, lng: -95.7129},
-            zoom: 3.5,
+            center: california,
+            zoom: 4,
             disableDefaultUI: true
         });
-    }
-    
+
+        var request = {
+            query: depart + "airport",
+            fields: ['name', 'geometry'],
+        };
+
+        service = new google.maps.places.PlacesService(map);
+
+        service.findPlaceFromQuery(request, function (results) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+
+            var latLngArrive = results[0].geometry.location;
+        });
+
+        var requestTwo = {
+            query: arrive + "airport",
+            fields: ['name', 'geometry'],
+        };
+
+        service.findPlaceFromQuery(requestTwo, function (results) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+                // console.log(results[i].geometry.location.lat);
+            }
+
+            var latLngDepart = results[0].geometry.location;
+
+        });
+
+        function createMarker(place) {
+            var marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            });
+
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.setContent(place.name);
+                infowindow.open(map, this);
+            });
+        }
+    };
+
 });
