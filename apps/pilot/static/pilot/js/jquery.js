f504afecde1333d92b96bad05e092925cb80ab35
Juan Martinez <juanmartinezv6@gmail.com>
2019-08-30 20:46:12 -0700

    added polyline, got heading and distance

--- apps/pilot/static/pilot/js/jquery.js
+++ apps/pilot/static/pilot/js/jquery.js
@@ -1,67 +1,78 @@
 $(document).ready(function () {
-
-    console.log(depart, arrive);
-
+    
     $("select").formSelect();
 
-    var map;
+    var marker1;
+    var marker2;
     var service;
-    var infowindow;
+    var serviceTwo
+    var poly;
+    var map;
 
-    window.initMap = initMap;
-
-    function initMap() {
+    window.initMap = function() {
+        
         var california = new google.maps.LatLng(37.0902, -102.7129);
-
-        infowindow = new google.maps.InfoWindow();
 
         map = new google.maps.Map(document.getElementById('map'), {
             center: california,
-            zoom: 4,
+            zoom: 5,
             disableDefaultUI: true
         });
 
         var request = {
             query: depart + "airport",
-            fields: ['name', 'geometry'],
+            fields: ['geometry'],
         };
-
-        service = new google.maps.places.PlacesService(map);
-
-        service.findPlaceFromQuery(request, function (results) {
-            for (var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
-            }
-
-            var latLngArrive = results[0].geometry.location;
-        });
 
         var requestTwo = {
             query: arrive + "airport",
-            fields: ['name', 'geometry'],
+            fields: ['geometry'],
         };
 
-        service.findPlaceFromQuery(requestTwo, function (results) {
-            for (var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
-                // console.log(results[i].geometry.location.lat);
-            }
+        service = new google.maps.places.PlacesService(map);
+        serviceTwo = new google.maps.places.PlacesService(map);
 
-            var latLngDepart = results[0].geometry.location;
+        service.findPlaceFromQuery(request, function (results) {
+            marker1 = new google.maps.Marker({
+                map: map,
+                draggable: true,
+                position: results[0].geometry.location
+            });
 
+            serviceTwo.findPlaceFromQuery(requestTwo, function (results) {
+                marker2 = new google.maps.Marker({
+                    map: map,
+                    draggable: true,
+                    position: results[0].geometry.location
+                });
+
+                var bounds = new google.maps.LatLngBounds(
+                    marker1.position, marker2.position);
+                map.fitBounds(bounds);
+
+                update();
+
+            });
         });
 
-        function createMarker(place) {
-            var marker = new google.maps.Marker({
-                map: map,
-                position: place.geometry.location
-            });
-
-            google.maps.event.addListener(marker, 'click', function () {
-                infowindow.setContent(place.name);
-                infowindow.open(map, this);
-            });
-        }
+        poly = new google.maps.Polyline({
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 3,
+            map: map,
+        });
     };
 
+    function update() {
+        var path = [marker1.getPosition(), marker2.getPosition()];
+        poly.setPath(path);
+        var heading = google.maps.geometry.spherical.computeHeading(path[0], path[1]);
+        var finaleHeading = Math.round(heading);
+        document.getElementById('heading').value = finaleHeading;
+        var d = google.maps.geometry.spherical.computeDistanceBetween(path[0], path[1]);
+        var x = d / 1852;
+        var finaleDistance = Math.round(x);
+        document.getElementById('distance').value = finaleDistance;
+      };
+      
 });
