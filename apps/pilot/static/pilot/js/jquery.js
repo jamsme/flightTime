3b54509f9a5e153df69aa077ff8d84c7cfd1e4a8
Juan Martinez <juanmartinezv6@gmail.com>
2019-09-04 21:11:41 -0700

    took the Latitude and Longitude of the depart airport and got the weather for that airport using openWeatherMap API

--- apps/pilot/static/pilot/js/jquery.js
+++ apps/pilot/static/pilot/js/jquery.js
@@ -32,10 +32,10 @@ $(document).ready(function () {
         service = new google.maps.places.PlacesService(map);
         serviceTwo = new google.maps.places.PlacesService(map);
 
-        service.findPlaceFromQuery(request, function (results) {
+        service.findPlaceFromQuery(request, function (result) {
             marker1 = new google.maps.Marker({
                 map: map,
-                position: results[0].geometry.location
+                position: result[0].geometry.location
             });
 
             serviceTwo.findPlaceFromQuery(requestTwo, function (results) {
@@ -49,7 +49,10 @@ $(document).ready(function () {
                 map.fitBounds(bounds);
 
                 update();
-
+                console.log(wind);
+                if (wind == "yes") {
+                    getWeather(getLatLng(marker1)[0], getLatLng(marker1)[1]);
+                };
             });
         });
 
@@ -59,6 +62,29 @@ $(document).ready(function () {
             strokeWeight: 3,
             map: map,
         });
+    };
+
+    function getWeather(x,y) {
+        var request = new XMLHttpRequest()
+        request.open('GET', `http://api.openweathermap.org/data/2.5/weather?lat=${x}&lon=${y}&appid=6f2782e4e065a6d3e06dbbbc35e40240`, true);
+        request.onload = function () {
+            var data = JSON.parse(this.response)
+            if (request.status >= 200 && request.status < 400) {
+                console.log("got data");
+              } else {
+                console.log('error')
+            }
+        }
+        request.send()
+    };
+
+    function getLatLng(x) {
+        var mark1 = String(x.getPosition());
+        var remove = mark1.replace("(","");
+        var remove2 = remove.replace(")","");
+        var split = remove2.split(",");
+        var latLng = [Number(split[0]), Number(split[1])];
+        return latLng;
     };
 
     function update() {
